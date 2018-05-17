1233443d901038eef9fdc2b1b11d054a309e7631
Ayoub Oudmane <ayoub@oudmane.me>
2018-05-17 18:59:12 +0000

    ! Commit

--- deploy.sh
+++ deploy.sh
@@ -4,10 +4,9 @@ rm -rf ./npm
 
 npm --no-git-tag-version version patch
 
-npm run build
+babel source --out-dir npm -D
 
 node -e "const package = require('./package.json'), fs = require('fs'); \
-    delete package.scripts; \
     fs.writeFileSync('./npm/package.json', JSON.stringify(package, null, 2));
 "
 
