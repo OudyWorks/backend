dafe60f4e720435b215b26baad429f22a7cc5131
Ayoub Oudmane <ayoub@oudmane.me>
2019-11-11 00:30:21 +0300

    cli

--- cli/src/index.js
+++ cli/src/index.js
@@ -44,7 +44,7 @@ program
   .option('-p, --port <n>', 'Port', 8080)
   .option('-h, --host [value]', 'Host', 'localhost')
   .option('-w, --websocket', 'WebSocket')
-  .option('-c, --cookies', 'Cookies')
+  .option('-c, --cookies [value]', 'Cookies', true)
   .action(function ({
     port,
     host,
